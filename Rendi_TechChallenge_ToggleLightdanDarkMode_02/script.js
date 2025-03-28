e62dbf5d814ee2a138873d0e32f9bee987724bd8
Rendi <rendi.wikarta@binus.ac.id>
2025-03-28 03:34:23 +0000

    Added comments in html css and js and changed js for ToggleLightdanDarkMode_02

--- Rendi_TechChallenge_ToggleLightdanDarkMode_02/script.js
+++ Rendi_TechChallenge_ToggleLightdanDarkMode_02/script.js
@@ -1,9 +1,15 @@
 
+// Alurnya adalah toggle class body yang dark menjadi light, dan light menjadi dark
+// Lalu sisa logikanya ada di css yang menggunakan css variables
 
-$(function() {
+// Saat semua html dan image sudah load, jalankan fungsi
+$(window).on("load", function() {
+    // Membuat variable untuk target tombol dan body
+    // Body akan diganti classnya
     let button = $("#theme");
     let body = $("body");
     button.on("click", function() {
+        // toggleClass digunakan untuk mengganti bolak-balik
         body.toggleClass("dark light");
     })
 })
